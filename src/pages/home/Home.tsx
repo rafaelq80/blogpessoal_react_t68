ac2ec360d565b6b4c32ba3096104223aa59204f5
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-15 12:08:30 -0300

    Componente Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+﻿
+function Home() {
+
+    return (
+        <>
+            <div id="container" style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div id="subcontainer">
+                    <div id="texto" style={{
+                        width: "80vw",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}>
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui os seus pensamentos e opiniões</p>
+                    </div>
+
+                    <div id="imagem" style={{
+                        width: "80vw",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}>
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
